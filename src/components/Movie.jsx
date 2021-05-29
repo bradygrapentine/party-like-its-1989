b40eadfb4e63681afb833846fd85f77f151fcdb3
bradygrapentine <brady.grapentine@gmail.com>
2021-05-29 15:34:14 -0400

    added input field for filtering

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -4,12 +4,13 @@ class Movie extends Component {
   render() {
     return (
       <li key={this.props.id}>
-        <img src={this.props.poster} />
-        <a
+        {/* <a
           href={`https://api.themoviedb.org/3/movie/${this.props.id}?api_key=44f9f452be88057e15959be8257069c0`}
-        >
-          {this.props.title}
-        </a>
+        > */}
+        {this.props.title}
+        {/* </a> */}
+        <img src={this.props.poster} />
+
         <h5>{this.props.releaseDate}</h5>
         <p>{this.props.overview}</p>
       </li>
